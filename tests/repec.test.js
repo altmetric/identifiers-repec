d893b94a2b0383f0c5d88c121c92c7f6375303fe
Jonathan Hernandez <jbilbo@gmail.com>
2017-05-08 10:02:22 +0200

    Implement RePEc extraction

---
+++ tests/repec.test.js
@@ -0,0 +1,22 @@
+/* globals test, expect */
+"use strict";
+
+const repec = require("../lib/repec");
+
+test("extracts RePEc IDs", () => {
+    var str = "RePEc:wbk:wbpubs:2266\nRePEc:inn:wpaper:2016-03";
+
+    expect(repec.extract(str)).toEqual(["RePEc:wbk:wbpubs:2266", "RePEc:inn:wpaper:2016-03"]);
+});
+
+test("normalizes RePec IDs", () => {
+    var str = "REPEC:wbk:wbpubs:2266\nrepec:inn:wpaper:2016-03";
+
+    expect(repec.extract(str)).toEqual(["RePEc:wbk:wbpubs:2266", "RePEc:inn:wpaper:2016-03"]);
+});
+
+test("extracts RePEc IDs separated by Unicode whitespace", () => {
+    var str = "RePEc:wbk:wbpubs:2266 RePEc:inn:wpaper:2016-03";
+
+    expect(repec.extract(str)).toEqual(["RePEc:wbk:wbpubs:2266", "RePEc:inn:wpaper:2016-03"]);
+});
